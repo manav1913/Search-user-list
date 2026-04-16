275563eb570d1a3ed4eb089f79d861888f7a10c7
manav1913 <69932908+manav1913@users.noreply.github.com>
2026-04-16 06:26:43 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Users from "./MovieComponents/Users"
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Users/>
+    </>
+  )
+}
+
+export default App
---
+++ src/MovieComponents/Users.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import users from './Data'
+
+const Users = () => {
+    const [userList, setuserList] = useState(users)
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Users
